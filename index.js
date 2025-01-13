90aba1100f4a6384eac5cc9e568f0dfefd2b0bda
beomki <beomki7429@gmail.com>
2025-01-13 16:20:30 +0900

    msg

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-var lang="ko";
-    window.onload = function()
-    {
-        var url = unescape(location.href);
-        var pm  = url.split('?');
+// var lang="ko";
+//     window.onload = function()
+//     {
+//         var url = unescape(location.href);
+//         var pm  = url.split('?');
 
-        var params   = pm[1].split('&');
-        for( var i=0; i<params.length; i++ )
-        var param = params[i].split('=');
-        if (param[0] === 'lang'){
-            lang=param[1];
-        }
-        document.getElementById(lang).className += ' active';
+//         var params   = pm[1].split('&');
+//         for( var i=0; i<params.length; i++ )
+//         var param = params[i].split('=');
+//         if (param[0] === 'lang'){
+//             lang=param[1];
+//         }
+//         document.getElementById(lang).className += ' active';
+//     }
+
+function telToggle() {
+    var x = document.getElementById("telToggle");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
+}
 
+document.addEventListener("DOMContentLoaded", function () {
+    var calendarEl = document.getElementById("calendar");
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        timeZone: "local",
+        initialDate: "2025-06-07",
+    });
+    calendar.render();
+});
